2b0e90e7d4701e863bf3ad51f5195022f28bdc88
geggit <ronbeli@hotmail.com>
2012-03-04 19:09:34 +0100

    Initial commit - Generated the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biletarii::Application.config.secret_token = '8aab4f257a9723b77a456019e97c49167a455932a4f279d769c75cb6d2d5cd3f4d70ac73839bdd6059b719db7c06068d5fa4d1d13cf10943075c09650c83c575'

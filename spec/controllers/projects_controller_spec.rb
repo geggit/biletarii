54e362495db42b184b035cf64ca5ae82e233880a
geggit <ronbeli@hotmail.com>
2012-03-13 00:22:22 +0100

    Restricting actions to admin only. 3 scenarios broken.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -7,18 +7,31 @@ describe ProjectsController do
       user
   end
 
+  let(:project) { Factory(:project) }
 
 
 
 
   context "standard users" do
-    it "cannot access the new action" do
-      sign_in(:user, user)
-      get :new
-      response.should redirect_to(root_path)
-      flash[:alert].should eql("You must be an admin to do that.")
+
+
+
+
+    { "new" => "get",
+      "create" => "post",
+      "edit" => "get",
+      "update" => "put",
+      "destroy" => "delete" }.each do |action, method|
+      it "cannot access the #{action} action" do
+        sign_in(:user, user)
+        send(method, action.dup, :id => project.id)
+        response.should redirect_to(root_path)
+        flash[:alert].should eql("You must be an admin to do that.")
+      end
     end
-  end
+        end
+
+
 
 
   it "displays an error for a missing project" do
